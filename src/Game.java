4c199b68fd9046f275a5a5f1bb622b46a5447532
Pavel Rashkov <pavel.r.rashkov@gmail.com>
2014-09-14 11:25:28 +0300

    game class change

--- src/Game.java
+++ src/Game.java
@@ -1,8 +1,12 @@
+import java.awt.BorderLayout;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Image;
 import java.awt.RenderingHints;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 
@@ -20,12 +24,16 @@ public class Game extends JPanel{
 		p = new Padle(this, 5 , 150);
 		p2 = new Padle(this, 200, 150);
 		JFrame panel = new JFrame("PingPong");
-		panel.add(this);
-		panel.setSize(300, 300);
+		panel.add(this);	
+		panel.setSize(700, 350);
 		panel.setVisible(true);
 		panel.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		InputHandler ih = new InputHandler();
 		panel.addKeyListener(ih);
+
+		ImagePanel pl = new ImagePanel("images/background.png");
+		panel.getContentPane().add(pl, BorderLayout.CENTER);
+		panel.setSize(600, 350);
 	
 	}
 	
@@ -44,21 +52,20 @@ public class Game extends JPanel{
 		b.paint(g2d);
 		p.paint(g2d);
 		p2.paint(g2d);
+		
 	}
 	
 	public static void main(String[] args) throws InterruptedException {
 		
 		game = new Game();
-		
+		game.setOpaque(false); //hides the background of game
 		
 		while(true){
 			
 			game.move();
 			game.repaint();
 			Thread.sleep(10);
-		}
-		
-	}
-		
+		}	
+	}	
 	
 }
