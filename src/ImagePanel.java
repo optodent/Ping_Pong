3b13176f1e38939e529ed5a967681bfcee9954cf
Pavel Rashkov <pavel.r.rashkov@gmail.com>
2014-09-14 11:21:37 +0300

    add ImagePanel class and background

---
+++ src/ImagePanel.java
@@ -0,0 +1,22 @@
+import java.awt.Graphics;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JPanel;
+
+
+public class ImagePanel extends JPanel {
+
+    private Image image = null;
+
+    public ImagePanel(String filename) {
+        this.image = new ImageIcon(filename).getImage();
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+       
+        g.drawImage(image, 0, 0, 600, 350, null);
+    }
+}
