50406c56beff99eb856d4b91c4838fdc359ca4e9
Dimitar Atanasov <dimmm_2000@yahoo.com>
2014-09-17 11:56:57 +0300

    Timer fixed

--- src/Timere.java
+++ src/Timere.java
@@ -4,26 +4,33 @@ import java.awt.font.TextAttribute;
 import java.text.AttributedString;
 
 public class Timere {
+	public Timere(){
+		startTime = System.currentTimeMillis();
+	}
 	private long currentTime = 0;
+	private long startTime = 0;
+	private long minutes = 0;
+	private long seconds = 0;
 	
 	void refresh() {
-		currentTime ++;
+		currentTime = startTime - System.currentTimeMillis();
+		minutes = currentTime/60000;
+		seconds = (currentTime%60000)/1000;
 	}
 	
 	public void paint(Graphics2D g) {
 		//The precedence is important. Do NOT remove the brackets.
-		long counter = currentTime / (1000 / Game.SLEEP_TIME);
-		long remainder = counter % 3600;
-		long minutes = remainder / 60;
-		long seconds = remainder % 60; 
+		//long counter = currentTime / (1000 / Game.SLEEP_TIME);
+		//long remainder = counter % 3600;
+		//long minutes = remainder / 60;
+		//long seconds = remainder % 60; 
 
-		String time = ( (minutes < 10 ? "0" : "") + minutes 
-				+ ":" + (seconds< 10 ? "0" : "") + seconds ); 
+		String time = String.format(minutes +":" + seconds); 
 
 		AttributedString as = new AttributedString(time);
 		as.addAttribute(TextAttribute.SIZE, 20);
 		as.addAttribute(TextAttribute.FOREGROUND, Color.white);
 		
-		g.drawString(as.getIterator(), 520, 30);
+		g.drawString(as.getIterator(), 620, 30);
 	}
 }
